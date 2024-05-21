27b4af90be0d4392722fcfb557b268120b87a76e
SSSSSGGGGGG <164677456+SSSSSGGGGGG@users.noreply.github.com>
2024-05-21 15:37:57 +0200

    Add files via upload

--- newsimulate a.py
+++ newsimulate a.py
@@ -8,10 +8,14 @@ Created on Sun May 12 17:46:53 2024
 import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
+from sympy import symbols, simplify
+import matplotlib as mpl
 
+
+mpl.rcParams['font.size'] = 15
 phi= sp.symbols('phi')
 # Define range for 'a'
-a_values = np.linspace(0, 1, 10)  # Adjust range as needed
+a_values = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.96,1.0]  # Adjust range as needed
 
 # Initialize an empty list to store solutions
 solutions = []
@@ -26,22 +30,87 @@ for a in a_values:
     # Solve equation and apply modulo operation
     equation = sp.Eq(f1, f0)
     solution = sp.solve(equation, phi)
-    print(solution,"...")
+    # print(solution,"...")
     # Append the first solution (if exists) to the list
     if solution:
         solutions.append(solution[0] % 2)
         solutions2.append(solution[1] % 2)
     else:
         solutions.append(None)  # Append None if no solution found
-        solutions.append(None)
+        solutions2.append(None)
 # Plot 'a' vs 'phi' for each solution
-print(solutions,"...",solutions2)
+new_sol1=np.array(solutions)
+new_sol1[10]=np.array(solutions2)[10]
+new_sol2=np.array(solutions2)
+new_sol2[10]=np.array(solutions)[10]
+print(new_sol1,"...",new_sol2)
 plt.figure(figsize=(10, 6))
-plt.plot(a_values, solutions, label='Solution')
-plt.plot(a_values, solutions2, label='Solution')
+plt.plot(a_values, new_sol1, label='Solution1')
+plt.plot(a_values, new_sol2, label='Solution2')
 plt.xlabel('a')
 plt.ylabel('phi (mod 2)')
 plt.title('Solutions mod 2 for Different Values of a')
 plt.legend()
+plt.xlim([0.1,1])
+plt.ylim([0.5,1.5])
+plt.yticks(ticks=np.arange(0.4,1.7,0.1))
+plt.grid(True)
+plt.show()
+
+sol_f0 = []
+sol_f1 = []
+sol2_f0 = []
+sol2_f1 = []
+sum1_tri = []
+sum2_tri = []
+sum1_tri_a=[]
+sum2_tri_a=[]
+# Iterate over the values of 'a'
+for j,i in enumerate(a_values) :
+    phi1 = new_sol1.astype(float)
+    phi2 = new_sol2.astype(float)
+    f0_inverse=i**2 * np.cos((phi1[j] / 2) * np.pi)**2
+    f1_inverse = i**2 * np.sinc((i / 2) )**2 * np.sin((phi1[j] / 2) * np.pi)**2
+    f0_inverse2=i**2 * np.cos((phi2[j] / 2) * np.pi)**2
+    f1_inverse2 = i**2 * np.sinc((i / 2) )**2 * np.sin((phi2[j] / 2) * np.pi)**2
+    print(f"var1={f0_inverse-f1_inverse},var2={f0_inverse2-f1_inverse2}")
+    sum1=f0_inverse+2*f1_inverse
+    sum2=f0_inverse2+2*f1_inverse2
+    sum1_tri.append(sum1)
+    sum2_tri.append(sum2)
+    sum1_tri_a.append(sum1/(i**2))
+    print(sum1/(i**2))
+    sum2_tri_a.append(sum2/(i**2))
+    sol_f0.append(f0_inverse)  # Append None if no solution found
+    sol_f1.append(f1_inverse)
+    sol2_f0.append(f0_inverse2)  # Append None if no solution found
+    sol2_f1.append(f0_inverse2)
+    
+# Print the dictionaries (optional)
+# print(f"I1_tri0={sol_f0},I1_tri1={sol_f1},I2_tri0={sol2_f0},I2_tri1={sol2_f1}")
+# print(f"I1_tri_tot={sum1_tri},I2_tri_tot={sum2_tri}")
+plt.figure(figsize=(10, 6))
+plt.plot(a_values, sum1_tri, label='diffraction efficiency for sol1',marker="*")
+plt.plot(a_values, sum2_tri, label='diffraction efficiency for sol2')
+plt.xlabel('a')
+plt.ylabel('different efficiency')
+plt.title('DE of triplicator for Different Values of a')
+plt.legend()
+plt.xlim([0.1,1])
+plt.minorticks_on()
+plt.yticks(ticks=np.arange(0,1.1,0.1))
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.plot(a_values, sum1_tri_a, label='diffraction efficiency for sol1',marker="*")
+plt.plot(a_values, sum2_tri_a, label='diffraction efficiency for sol2')
+plt.xlabel('a')
+plt.ylabel('diffraction efficiency')
+plt.title('DE of triplicator for Different Values of a is divided by the square of a')
+plt.legend()
+plt.xlim([0.1,1])
+plt.minorticks_on()
+plt.yticks(ticks=np.arange(0.8,1.5,0.1))
 plt.grid(True)
 plt.show()
---
+++ simulation of derivation without envelope.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 10:08:29 2024
+
+@author: gaosh
+"""
+import numpy as np
+from scipy import special
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import sympy as sp
+from sympy import latex
+
+from sympy import init_printing
+
+# Define symbolic variables
+N,delta,u,a, phi = sp.symbols('N delta u a phi')
+# N=m+kn
+
+# Define the constraint equation
+n = sp.Symbol('n', integer=True)
+k = sp.Symbol('k', integer=True) 
+m = sp.Symbol('m', integer=True) # Define n as an integer
+# constraint_eq = sp.Eq(n + k * m, sp.Integer(0))
+
+# Define the expression to be summed
+expr = (-1)**n * a * (-sp.sin(sp.pi * m / 2) * sp.sin(sp.pi * phi / 2) + sp.cos(sp.pi * m / 2) * sp.cos(sp.pi * phi / 2)) * sp.sinc((m/2)) *sp.DiracDelta(u-(N/(k*delta)))
+
+# Compute the summation for each solution of n
+
+result_sub=expr.subs({k:8,N:5,m:5-8*n}) #!!!! change N and m at the same time.
+result = sp.Sum(sp.Sum(result_sub, (n, -sp.oo, sp.oo)),(m, -sp.oo, sp.oo)) #(n, -sp.oo, sp.oo)),(m, -sp.oo, sp.oo)
+# result=sum(result_sub for n in range(-100, 101) for m in range(-100, 101))
+#final_result = sum(result_sub.subs({n: i}) for i in range(-50, 50))# numerical evaluate
+
+G=sp.simplify(result)
+print("G=")
+# sp.pprint(sp.simplify(result_sub))
+sp.pprint(G)
+# sp.pprint(result_sub.doit().simplify())
+# sp.pprint(result_sub.evalf())
+print("I=")
+
+sp.pprint(G**2)
+
+expr_latex=sp.latex(result_sub**2)
+# print(expr_latex)
+combined1 = r'$\sum_{-\infty \leq n \leq \infty, -\infty \leq m \leq \infty}'
+G_latex =combined1 +" "+ expr_latex +"$"
+
+#G_latex=r'$\sum_{-\infty \leq n \leq \infty, -\infty \leq m \leq \infty}\ a *\cos{\left(\frac{\pi \phi}{2} \right)} \delta\left(u\right) \operatorname{sinc}{\left(n \right)}$'
+plt.text(-0.15, 0.5,G_latex, fontsize=15, ha='left') #r'$%s$' % expr_latex
+plt.axis('off')  # Turn off axes
+plt.savefig('expression_image.jpg')
+plt.show()
+
+
+
+
+# print(f"the period is {k}, the order {N} is calculated, m={m} is for grating order, and n={n} is for SLM.")
+# print(f"the spatial frequences are {u}")
+
+# a_sinc=a*sp.sinc((a/k)*(m+k*n))
+# cos=sp.cos(0.5*m*sp.pi)*sp.cos(0.5*phi*sp.pi)
+# sin=sp.sin(0.5*m*sp.pi)*sp.sin(0.5*phi*sp.pi)
+# diff=cos-sin
+
+# G=a_sinc*diff*sp.sinc(0.5*m)*pow(-1.0,n)
+
+# print(G)
+# G_sum=sp.Sum(G)
+# Intensity=G_sum**2
+
+
